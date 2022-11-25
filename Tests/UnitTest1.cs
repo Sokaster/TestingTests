b3e0153973f7ab4ee9578a4d2c55582e1cf5e910
‘igorslepenkov’ <slepenkov.nii@yandex.by>
2022-11-25 21:21:40 +0300

    da

---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,37 @@
+using System.Security.Cryptography.X509Certificates;
+using UnitTestExample;
+namespace Tests
+{
+    public class TaxCalculatorTests
+    {
+        [Fact]
+        public void Calculator_OK()
+        {
+            //AAA
+            //1.Arrange
+            var car = new Car()
+            {
+                Model = "406",
+                Maker = "Peugeot",
+                Year = 1999,
+                EngineCapacity = 2000,
+                Price = 6000
+            };
+            //Act
+            var result = new TaxCalculator().Calculate(car);
+
+            //Assert
+            Assert.Equal(3000, result.BorderTax);
+            Assert.Equal(1000, result.PollutionTax);
+            Assert.Equal(1200, result.VatTax);
+           
+            
+
+        }
+        [Fact]
+        public void Calculator_InvalidParameter_Fail()
+        {
+            Assert.Throws<ParameterInvalidException>(() => new TaxCalculator().Calculate(null));
+        }
+    }
+}
